bec594bcd6d27e81c3c46af7827ecacbe5c89c0f
JeanC09WTF <gutierrezjean484@gmail.com>
2023-12-10 21:42:09 -0600

    Paginas de login y informacion listas

--- src/app/registrarse/registrarse.page.ts
+++ src/app/registrarse/registrarse.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registrarse',
@@ -10,14 +11,23 @@ export class RegistrarsePage implements OnInit {
 
   token= 'dsgf24e2fsd';
 
-  constructor( private router: Router ) { }
+  constructor( private router: Router, public toastController: ToastController ) { }
 
   ngOnInit() {
   }
-
+  /**Funcion de registrarse*/
   registrarse(){
     localStorage.setItem('token', this.token)
     this.router.navigate(["/inicio"])
+  }
+/**Alerta de registro*/
+  async presentToast1(){
+    const toast = await this.toastController.create({
+      message: 'Registro Exitoso',
+      duration: 2000,
+      position: "middle",
+    });
+    toast.present()
   }
 
 }
